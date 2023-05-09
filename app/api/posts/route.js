8fd4d0613b5e0cfec46dca2f5fdcf86759e48227
RealShinyHand <start_so@naver.com>
2023-05-09 10:56:57 +0900

    delete next.config.js for security

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,11 +1,37 @@
-
+import { connectDB } from "@/util/connectDB";
+import { NextResponse } from "next/server";
 export async function POST(req){
     console.log(typeof(req));
 
     const formData = await req.formData();
     console.log(formData.get("title"));
     console.log(formData.get("content"));
-    return new Response('hi',{
-        status:200
-    });
+    const title = formData.get("title");
+    const content = formData.get("content");
+
+    try {
+        const db = (await connectDB).db('forum');
+
+        const postCollection = db.collection("post");
+        // create a document to insert
+        const doc = {
+          'title': title,
+          'content': content
+        }
+        if(doc.title == null || content == null){
+            throw new Error('Parameter is not fully saticified');
+        }
+        const result = await postCollection.insertOne(doc);
+        console.log(result);
+      
+    }catch( e){
+        alert(e);
+        console.log(e);
+    } 
+      
+    NextResponse.redirect("/list");
+}
+
+export async function GET(req){
+
 }
